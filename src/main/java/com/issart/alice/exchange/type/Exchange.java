edea2a4af558a242817d67bb421bb0d1b5205486
Artem Golubnichenko <agolubnichenko@issart.com>
2018-12-08 13:10:38 +0600

    Added codes for the Exchange

--- src/main/java/com/issart/alice/exchange/type/Exchange.java
+++ src/main/java/com/issart/alice/exchange/type/Exchange.java
@@ -2,19 +2,25 @@ package com.issart.alice.exchange.type;
 
 public enum Exchange {
 
-    NYSE("Нью-Йоркская фондовая биржа"),
-    NASDAQ("NASDAQ"),
-    LSE("Лондонская фондовая биржа"),
-    MMVB("Московская биржа"),
-    RTS("Российская торговая система");
+    NYSE("Нью-Йоркская фондовая биржа", ""),
+    NASDAQ("NASDAQ", "Nasdaq"), // https://www.rbc.ru/ajax/indicators
+    LSE("Лондонская фондовая биржа", ""),
+    IMOEX("Московская биржа", "IMOEX"), // https://www.rbc.ru/ajax/indicators
+    RTSI("Российская торговая система", "RTSI"); // https://www.rbc.ru/ajax/indicators
 
     private String name;
+    private String code;
 
-    Exchange(String name) {
+    Exchange(String name, String code) {
         this.name = name;
+        this.code = code;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getCode() {
+        return code;
     }
 }
