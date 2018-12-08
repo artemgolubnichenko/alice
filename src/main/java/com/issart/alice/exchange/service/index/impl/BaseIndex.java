736434ad79237f5793d41dddcf178139657fb0c5
Artem Golubnichenko <artem.golubnichenko@upandrunningsoftware.com>
2018-12-08 13:09:34 +0600

    Added base class for the exchange

---
+++ src/main/java/com/issart/alice/exchange/service/index/impl/BaseIndex.java
@@ -0,0 +1,72 @@
+package com.issart.alice.exchange.service.index.impl;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.stream.Collectors;
+import com.issart.alice.exchange.service.currency.impl.BaseCurrency;
+import com.issart.alice.exchange.service.index.IIndex;
+import com.issart.alice.exchange.type.Exchange;
+import com.issart.alice.exchange.type.ExchangeInfo;
+import io.vertx.core.json.Json;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.apache.log4j.Logger;
+
+import static java.util.concurrent.TimeUnit.MINUTES;
+
+public abstract class BaseIndex implements IIndex {
+
+    protected Map<Exchange, ExchangeInfo> exchangeInfoMap = new HashMap<>();
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    public static final  String RESOURCE_URL = "https://www.cbr-xml-daily.ru/daily_json.js";
+    private final static Logger LOGGER = Logger.getLogger(BaseCurrency.class);
+
+    public BaseIndex() {
+        scheduler.scheduleAtFixedRate(() -> {
+            pull();
+            LOGGER.info("Pulling for currencies..");
+        }, 0, 30, MINUTES);
+    }
+
+
+    @Override
+    public ExchangeInfo getIndex(Exchange exchange) {
+        return exchangeInfoMap.get(getExchangeCode());
+    }
+
+    public abstract Exchange getExchangeCode();
+
+    private void pull() {
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            HttpGet request = new HttpGet(RESOURCE_URL);
+            request.addHeader("content-type", "application/json");
+            HttpResponse result = httpClient.execute(request);
+            String json = EntityUtils.toString(result.getEntity(), "UTF-8");
+            CurrencyResponse response = Json.decodeValue(json, CurrencyResponse.class);
+            Map<String, Valute> map = response.getValutes();
+            Set<String> availableCarriencies = new HashSet<>(Arrays.asList(Currency.values()))
+                .stream()
+                .map(Currency::getCode)
+                .collect(Collectors.toSet());
+            for (Map.Entry<String, Valute> entry : map.entrySet())
+            {
+                if(availableCarriencies.contains(entry.getKey())) {
+                    Valute valute = entry.getValue();
+                    ExchangeInfo info = new ExchangeInfo(valute.getValue(), valute.getPrevious());
+                    currencyInfoMap.put(Currency.valueOf(entry.getKey()), info);
+                }
+            }
+        } catch (IOException ex) {
+            LOGGER.error("Can't pull currencies: " + ex.getMessage(), ex);
+        }
+    }
+}
